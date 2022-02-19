d4f7d9d53666001535b0fe33f3fdcd8b92876b1b
Lenovo <Lenovo@DESKTOP-ACLM3M9>
2022-02-19 18:59:02 +0530

    Add project files.

---
+++ BMI/BMI.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveExpressions.Properties;
+using Microsoft.Bot.Builder.Dialogs;
+using Newtonsoft.Json;
+
+namespace BMI
+{
+    //Custom action
+    public class BMI : Dialog
+    {
+        public BMI([CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0) : base()
+        {
+            RegisterSourceLocation(sourceFilePath, sourceLineNumber);
+        }
+
+        [JsonProperty("$Kind")]
+        public const string Kind = "BMI";
+
+        [JsonProperty("Weight")]
+        public NumberExpression Weight { get; set; }
+
+        [JsonProperty("Height")]
+        public NumberExpression Height { get; set; }
+
+        [JsonProperty("resultProperty")]
+        public StringExpression ResultProperty { get; set; }
+
+        public override Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+
+            var w = Weight.GetValue(dc.State);
+            var h = Height.GetValue(dc.State);
+            var h2 = h * h;
+
+            var ans = Convert.ToDecimal(w) / Convert.ToDecimal(h2);
+            var result = Convert.ToDecimal(ans) * 10000;
+
+            if (ResultProperty != null)
+            {
+                dc.State.SetValue(this.ResultProperty.GetValue(dc.State), result);
+            }
+
+            return dc.EndDialogAsync(result: result, cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ IWF/IWF.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveExpressions.Properties;
+using Microsoft.Bot.Builder.Dialogs;
+using Newtonsoft.Json;
+
+namespace IWF
+{
+    //Custom action
+    public class IWF : Dialog
+    {
+        public IWF([CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0) : base()
+        {
+            RegisterSourceLocation(sourceFilePath, sourceLineNumber);
+        }
+
+        [JsonProperty("$Kind")]
+        public const string Kind = "IWF";
+
+        [JsonProperty("Height")]
+        public NumberExpression Height { get; set; }
+
+        [JsonProperty("resultProperty")]
+        public StringExpression ResultProperty { get; set; }
+
+        public override Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            var h = Height.GetValue(dc.State);
+
+            var i = Convert.ToDouble(h) * 0.0328084;
+            var j = (i * 12) - 60;
+            var k = j * 2.3;
+
+            var result = k + 45.5;
+
+            if (ResultProperty != null)
+            {
+                dc.State.SetValue(this.ResultProperty.GetValue(dc.State), result);
+            }
+
+            return dc.EndDialogAsync(result: result, cancellationToken: cancellationToken);
+        }
+    }
+}
